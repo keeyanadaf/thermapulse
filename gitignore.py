833e2e2e2a503bfdd359d9cc935b933adc96b037
keeyanadaf <keeyannadaf@gmail.com>
2024-12-02 23:10:54 +0530

    Initial commit

---
+++ gitignore.py
@@ -0,0 +1,24 @@
+# Ignore system files
+.DS_Store
+Thumbs.db
+
+# Ignore user-specific files
+.vscode/
+.streamlit/
+
+# Ignore Python virtual environments
+myenv/
+.env
+
+# Ignore temporary files
+*.log
+*.bak
+
+# Ignore directories like AppData or Documents
+AppData/
+Documents/
+Downloads/
+Favorites/
+Pictures/
+Music/
+Videos/
